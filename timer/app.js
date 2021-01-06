1f43b00a546554514ff85d9dd1c56fc7058cd589
Spicy-Raccoon <kassiustilia@yahoo.com>
2021-01-06 12:56:14 +0100

    Timer finished

--- timer/app.js
+++ timer/app.js
@@ -1,17 +1,22 @@
-class Timer {
-  constructor(durationInput, startButton, stopButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.stopButton = stopButton;
-    this.startButton.addEventListener('click', this.start);
-  }
-  start() {
-    console.log('timer started')
-  }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const stopButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
-const timer = new Timer(durationInput, startButton, stopButton);
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
+const timer = new Timer(durationInput, startButton, stopButton, {
+  onStart(totalDuration){
+    duration=totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute('stroke-dashoffset',
+    perimeter * timeRemaining / duration - perimeter
+   );
+  },
+  onComplete() {
+    console.log('Timer completed')
+  }
+});
