a66080e38cde8716f2eaaef226396585188fca04
Spicy-Raccoon <>
2021-01-06 14:00:46 +0100

    commented timer

--- timer/app.js
+++ timer/app.js
@@ -8,10 +8,12 @@ circle.setAttribute('stroke-dasharray', perimeter);
 
 let duration;
 const timer = new Timer(durationInput, startButton, stopButton, {
+  //the callbacks are not set in the Timer class, set them when creating the new Timer object
   onStart(totalDuration){
     duration=totalDuration;
   },
   onTick(timeRemaining) {
+    //move the circle around the timer every tick event
     circle.setAttribute('stroke-dashoffset',
     perimeter * timeRemaining / duration - perimeter
    );
--- timer/timer.js
+++ timer/timer.js
@@ -1,4 +1,5 @@
 class Timer {
+  // callbacks are optional, passed as an object with three properties. timer will work without it
   constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
@@ -8,9 +9,11 @@ class Timer {
       this.onTick = callbacks.onTick;
       this.onComplete = callbacks.onComplete;
     }
+    //add click events to run start() and pause() functions
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
   }
+  //used arrow functions for 'this' to refer to the timer object
   start = () => {
     if (this.onStart) {
       this.onStart(this.timeRemaining);
@@ -28,12 +31,14 @@ class Timer {
         this.onComplete();
       }
     } else {
+      //set the difference in timeRemaining == to the setInterval milliseconds in start()
       this.timeRemaining = this.timeRemaining-0.01;
       if (this.onTick) {
         this.onTick(this.timeRemaining)
       }
     }
   };
+  //get the time remaining to the duration input and set to the time passed, use as attributes later in the code
   get timeRemaining() {
     return parseFloat(this.durationInput.value);
   };
