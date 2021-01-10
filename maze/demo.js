64f27fe727cac45f8c4c2ae4d3a52bca01451f7d
Spicy-Raccoon <kas.lipko@yahoo.com>
2021-01-10 13:22:18 +0100

    grid created

--- maze/demo.js
+++ maze/demo.js
@@ -2,9 +2,9 @@
 //documentation for Matter JS at: https://brm.io/matter-js/
 
 //import some of the elements from Matter JS library
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Mouse, MouseConstraint } = Matter;
 
-const width = 600;
+const width = 800;
 const height = 600;
 //boilerplate code to create a basic Matter canvas
 //create an engine. with the engine you create a world object
@@ -25,8 +25,13 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
+//add Mouse click and drag capability
+World.add(world, MouseConstraint.create(engine, {
+  mouse: Mouse.create(render.canvas)
+}));
+
 //to add shapes to the canvas
-//(posx,posy,sizex,sizey, {options})
+//(pos,pos,size,size, {options})
 // const shape = Bodies.rectangle(200,200,50,50, {
 //   // isStatic: true //by default gravity is on in Matter
 // });
@@ -34,25 +39,24 @@ Runner.run(Runner.create(), engine);
 
 //Create Walls in the canvas
 const walls = [
-  Bodies.rectangle(width/2,0,width,40,{isStatic:true}),
-  Bodies.rectangle(width/2,height,width,40,{isStatic:true}),
-  Bodies.rectangle(0,height/2,40,height,{isStatic:true}),
-  Bodies.rectangle(width,height/2,40,height,{isStatic:true})
+  Bodies.rectangle(400,0,800,40,{isStatic:true}),
+  Bodies.rectangle(400,600,800,40,{isStatic:true}),
+  Bodies.rectangle(0,300,40,600,{isStatic:true}),
+  Bodies.rectangle(800,300,40,600,{isStatic:true})
 ];
-//can pass arrays in World.add()
+//can pass arrays in World.add
 World.add(world, walls)
 
-//Maze Generation
+//Random Shapes generator
 
-// with for loop for a 3x3 grid
-// const grid = [];
-// for (let i = 0; i < 3; i++) {
-//   grid.push([]);
-//   for (let j = 0; j < 3; j++) {
-//     grid[i].push(false);}}
-
-//concise way
-const grid = Array(3).fill(null).map(()=>Array(3).fill(false));
-//vertical and horizontal lines of the grid
-const verticals = Array(3).fill(null).map(()=>Array(2).fill(false));
-const horizontals = Array(3).fill(null).map(()=>Array(3).fill(false));
+for (var i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    World.add(world, Bodies.rectangle(Math.random()*width, Math.random()*height, 50, 50));
+  } else {
+    World.add(world, Bodies.circle(Math.random()*width, Math.random()*height, 30, {
+      render: {
+        fillStyle: 'red'
+      }
+    }));
+  }
+}
